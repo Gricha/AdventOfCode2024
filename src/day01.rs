95dda8265b428ea75ad99298fa950c96e35ab448
Greg Pstrucha <875316+Gricha@users.noreply.github.com>
2024-12-01 08:54:28 -0800

    day1

--- src/day01.rs
+++ src/day01.rs
@@ -1,3 +1,51 @@
-pub fn part1() {}
+use std::collections::HashMap;
 
-pub fn part2() {}
+use itertools::Itertools;
+
+use crate::utils::read_input;
+
+pub fn part1() {
+    let input = read_input("inputs/01_1.txt")
+        .into_iter()
+        .map(|x| {
+            x.split_whitespace()
+                .collect_vec()
+                .into_iter()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect_tuple::<(i32, i32)>()
+                .unwrap()
+        })
+        .collect_vec();
+    let list1 = input.iter().map(|(x, _)| x).sorted().collect_vec();
+    let list2 = input.iter().map(|(_, x)| x).sorted().collect_vec();
+    let result = list1
+        .into_iter()
+        .zip(list2)
+        .map(|(x, y)| x - y)
+        .map(|x| x.abs())
+        .sum::<i32>();
+    println!("{}", result);
+}
+
+pub fn part2() {
+    let input = read_input("inputs/01_1.txt")
+        .into_iter()
+        .map(|x| {
+            x.split_whitespace()
+                .collect_vec()
+                .into_iter()
+                .map(|x| x.parse::<i32>().unwrap())
+                .collect_tuple::<(i32, i32)>()
+                .unwrap()
+        })
+        .collect_vec();
+    let mut right = HashMap::new();
+    for (_, y) in &input {
+        *right.entry(*y).or_insert(0) += 1;
+    }
+
+    let result = input
+        .into_iter()
+        .fold(0, |acc, (k, _)| acc + (k * right.get(&k).unwrap_or(&0)));
+    println!("{}", result);
+}
